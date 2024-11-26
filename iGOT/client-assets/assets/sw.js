4df05076f897f412658c05170539d0ec5f076d0f
vishnubansaltarento <vishnu.bansal@tarento.com>
2024-11-26 10:29:10 +0530

    update for net core changes

---
+++ iGOT/client-assets/assets/sw.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: 'AIzaSyD8hKHYfT-5xuDR7QFtNLSMcg1i9jrrZgg',
+    messagingSenderId: '241942387335',
+    appId: '1:241942387335:web:0da83fd1e33496933d7622',
+    projectId: 'igot-karmayogi-689be',
+    user_key: 'ADGMOT35CHFLVDHBJNIG50K968HALK3BMP0VCCVVE0PODR835I00',
+    siteid: 'ca47dda1f1dbe252f37ebf86c2b4011f',
+    idc: 'tin'
+};
+importScripts('//cdnt.netcoresmartech.com/swv4.js');
