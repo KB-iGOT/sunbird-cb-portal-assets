e7d00c20721b02acced152514678578e8f8e1e5d
vishnubansaltarento <vishnu.bansal@tarento.com>
2024-12-05 14:34:51 +0530

    change for netcore

--- iGOT/client-assets/assets/sw.js
+++ iGOT/client-assets/assets/sw.js
@@ -4,6 +4,6 @@ var config = {
     appId: '1:241942387335:web:0da83fd1e33496933d7622',
     projectId: 'igot-karmayogi-689be',
     user_key: 'ADGMOT35CHFLVDHBJNIG50K9699EAVK6N6CDIHORCMKEF2V0ANSG',
-    siteid: '1ec8dc82675f25ce596016f2fc7d3848'
+    siteid: 'a477b639a08a5ee76917bf91ec97921a'
 };
 importScripts('//cdnt.netcoresmartech.com/swv4.js');
